bf4c4ab347f1c5633a0b12b9b7903e99bd5763dd
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-03-28 08:42:57 +0100

    Init

---
+++ main.py
@@ -0,0 +1,11 @@
+class Literals :
+        
+    def __init__(self, name, neg):
+        self.name = name
+        self.neg = neg 
+
+class Rules:
+
+    def __init__(self, name, defeasible, ):
+        self.name = name
+        self.defeasible = defeasible 
